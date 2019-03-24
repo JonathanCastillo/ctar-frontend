a359050ba1e87f7243fb30fb0446b467089a2947
Jonathan Castillo <JonaCastillo96@hotmail.com>
2019-03-24 12:02:47 -0600

    Servicio actualizado

--- src/app/services/articulos.service.ts
+++ src/app/services/articulos.service.ts
@@ -5,12 +5,25 @@ import { Injectable } from '@angular/core';
 })
 export class ArticulosService {
   private articulos: any[] = [
-    { nombre: 'Articulo 1',
-    descripcion: 'Desceipcion',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-wioAwiZFD7_hDLCQu_zhT4wghh_VGbJrW_1QbN4ZAhl9Bzhh',
+    { nombre: 'Camiseta de manta',
+    descripcion: 'Camiseta de manta, con finos bordados hechos a mano',
+    img: 'assets/img/manta.PNG',
     precio: '$44.50',
     marca: 'Calvin Klein'
+    },
+    { nombre: 'Colgantes de jade',
+    descripcion: 'Colgantes de jade, con incrustaciones de oro y plata en el borde',
+    img: 'assets/img/aritojade.jpg',
+    precio: '$30.00',
+    marca: 'Calvin Klein'
+    },
+    { nombre: 'Mariposas de barro',
+    descripcion: 'Mariposasa de barro pintadas a mano. Ideales para decorar salas de star',
+    img: 'assets/img/mariposas.jpg',
+    precio: '$20.00',
+    marca: 'Calvin Klein'
     }
+    
       ];
       getArticulos() {
         return this.articulos;
